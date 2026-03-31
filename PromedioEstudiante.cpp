61fcda642ba6b69b36d89b0112e1778df72ecbd2
Ali-20-l <aortiz5568@uta.edu.ec>
2026-03-31 11:10:26 -0500

    agrego mi parte

--- PromedioEstudiante.cpp
+++ PromedioEstudiante.cpp
@@ -1,9 +1,14 @@
+<<<<<<< HEAD
 #include <iostream>
 #include <string>
 #include <list>
 #include <iomanip>
 
 using namespace std;
+=======
+#include "Estudiante.h"
+
+>>>>>>> 86b6f11 (agrego mi parte)
 void promedioEstudiante() {
     limpiarPantalla();
     cout << "=== PROMEDIO DE NOTAS DE UN ESTUDIANTE ===" << endl;
@@ -13,6 +18,7 @@ void promedioEstudiante() {
     cin >> cedula;
     cin.ignore(1000, '\n');
 
+<<<<<<< HEAD
     // Busca en la lista de estudiantes usando el iterador
     list<Estudiante>::iterator it = buscarEstudiantePorCedula(cedula);
 
@@ -22,13 +28,24 @@ void promedioEstudiante() {
              << cedula << endl;
     } else {
         // Mostrar datos del estudiante
+=======
+    list<Estudiante>::iterator it = buscarEstudiantePorCedula(cedula);
+
+    if (it == listaEstudiantes.end()) {
+        cout << "\n[!] Error: No se encontro un estudiante con cedula: "
+             << cedula << endl;
+    } else {
+>>>>>>> 86b6f11 (agrego mi parte)
         cout << "\n  Nombres  : " << it->nombres << " " << it->apellidos << endl;
         cout << "  Edad     : " << it->calcularEdad() << " anios" << endl;
 
         if (it->notas.empty()) {
             cout << "  Promedio : (Sin calificaciones registradas)" << endl;
         } else {
+<<<<<<< HEAD
             // Recorrer la lista de notas para mostrarlas
+=======
+>>>>>>> 86b6f11 (agrego mi parte)
             cout << "  Calificaciones: ";
             bool primero = true;
             for (list<double>::iterator nit = it->notas.begin();
@@ -38,8 +55,11 @@ void promedioEstudiante() {
                 primero = false;
             }
             cout << endl;
+<<<<<<< HEAD
 
             // calcularPromedio() ya recorre internamente la lista de notas
+=======
+>>>>>>> 86b6f11 (agrego mi parte)
             cout << "  Promedio : " << fixed << setprecision(2)
                  << it->calcularPromedio() << endl;
         }
